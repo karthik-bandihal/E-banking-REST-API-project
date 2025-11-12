e42e039e40a699348712fec45d2250ef66716a4f
Karthik Bandihal <bandihalkarthik@gmail.com>
2025-11-13 00:43:51 +0530

    swagger configuartion

--- src/main/java/com/jsp/E_Banking/config/Security_Config.java
+++ src/main/java/com/jsp/E_Banking/config/Security_Config.java
@@ -9,16 +9,13 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class Security_Config {
-	
-	private String[] swaggerPaths = { "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**", "/webjars/**" };
-	
-	
-    @Bean
-	SecurityFilterChain security(HttpSecurity httpSecurity)throws Exception{
-    	httpSecurity.authorizeHttpRequests(x->x.requestMatchers(swaggerPaths).permitAll().anyRequest().authenticated());
-		httpSecurity.formLogin(x->x.disable());//from login disabling
-		httpSecurity.httpBasic(x->x.disable());// basic login pop msg disabled
-		return httpSecurity.build();
-	}
 
+	private String[] swaggerPaths = { "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**", "/webjars/**" };
+
+	@Bean
+	SecurityFilterChain security(HttpSecurity httpSecurity) throws Exception {
+		return httpSecurity
+				.authorizeHttpRequests(x -> x.requestMatchers(swaggerPaths).permitAll().anyRequest().authenticated())
+				.formLogin(x -> x.disable()).httpBasic(x -> x.disable()).build();
+	}
 }
